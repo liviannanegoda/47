f7ffe76b90df64ee1cf0ba00cf6e07ff07813cbe
liviannanegoda <135698720+liviannanegoda@users.noreply.github.com>
2025-05-29 14:35:13 +0300

    Create HomeworkNotesApplication.java

---
+++ src/main/java/com/github/Lidia/service/impl/HomeworkNotesApplication.java
@@ -0,0 +1,13 @@
+package com.github.yuliyaks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkNotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkNotesApplication.class, args);
+    }
+
+}
