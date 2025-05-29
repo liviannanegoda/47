c7fa43ef70e59b334105d6a5cc03c491a248a661
liviannanegoda <135698720+liviannanegoda@users.noreply.github.com>
2025-05-29 14:39:03 +0300

    Update NoteRepository.java

--- src/main/java/com/github/Lidia/repository/NoteRepository.java
+++ src/main/java/com/github/Lidia/repository/NoteRepository.java
@@ -1,6 +1,6 @@
-package com.github.yuliyaks.repository;
+package com.github.Lidia.repository;
 
-import com.github.yuliyaks.model.Note;
+import com.github.Lidia.model.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
