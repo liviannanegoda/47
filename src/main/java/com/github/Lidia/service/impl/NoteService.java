3eb59940a5fbbfc416d13e3e7e034ae85d212d19
liviannanegoda <135698720+liviannanegoda@users.noreply.github.com>
2025-05-29 14:34:37 +0300

    Create NoteService.java

---
+++ src/main/java/com/github/Lidia/service/impl/NoteService.java
@@ -0,0 +1,19 @@
+package com.github.yuliyaks.service;
+
+import com.github.yuliyaks.model.Note;
+
+import java.util.List;
+
+public interface NoteService {
+
+    Note createNote(Note note);
+
+    List<Note> getAllNotes();
+
+    Note getNoteById(Long id);
+
+    Note updateNote(Note note);
+
+    void deleteNote(Long id);
+
+}
